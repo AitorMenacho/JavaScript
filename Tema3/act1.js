0af13960e0af440616c42d4ab712434abd5d942d
Aitor Menacho <menacho.aitor@gmail.com>
2022-09-29 12:28:57 +0200

    act3

---
+++ Tema3/act1.js
@@ -0,0 +1,30 @@
+
+
+function inicializar() {
+
+    console.log("Hola");
+
+    document.getElementById("crear-ventana").onclick  = crearNueva;
+    document.getElementById("cerrar-ventana").onclick = cerrarNueva;
+
+
+}
+
+var nuevaVentana;
+
+function crearNueva() {
+
+    nuevaVentana = window.open("http://www.google.es", "", "height = 400, width = 800");
+
+}
+
+function cerrarNueva() {
+
+    if (nuevaVentana) {
+
+        nuevaVentana.close();
+        nuevaVentana = null;
+    
+    }
+
+}
