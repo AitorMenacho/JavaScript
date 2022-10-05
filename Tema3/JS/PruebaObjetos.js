79ba7e8737d8ef91d3fac136004763ee4d584051
Aitor Menacho <menacho.aitor@gmail.com>
2022-10-05 09:08:16 +0200

    Update PruebaObjetos.js

--- Tema3/JS/PruebaObjetos.js
+++ Tema3/JS/PruebaObjetos.js
@@ -9,6 +9,7 @@ console.log(cadena);
 
 // console.log("Longitud: " + cadena.length);
 
+// ==========================================================================
 
 /* toUpperCase() y toLowerCase() */
 
@@ -20,6 +21,8 @@ console.log(cadena);
 
 // console.log(cadena.toLocaleLowerCase());
 
+// ==========================================================================
+
 /* indexOf() */
 
 /* MODO Propiedad indexOf() -> Devulve la posición de la letra en la cadena, si no la encuentra devuelve -1 */
@@ -29,16 +32,71 @@ console.log(cadena);
 // console.log(cadena.indexOf('d'));
 // console.log(cadena.indexOf('ñ'));
 
+// ==========================================================================
+
 /* replace() */
 
 /* MODO Propiedad replace() -> Reemplaza una subcadena por otra. */
 
 // console.log(cadena.replace(' ', '...'));
 
+// ==========================================================================
+
 /* substring() */
 
-/* MODO Propiedad substring() -> Extrae los caracteres de una cadena entre dos números */
+/* MODO Propiedad substring() -> Extrae los caracteres de una cadena entre dos números.
+                                 En caso de no darle dos, empezara de ahí en adelante */
 
-console.log(cadena.substring(0,3));
+// console.log(cadena.substring(0,3));
 
-/*  */
+// console.log(cadena.substring(5));
+
+// ==========================================================================
+
+/* slice() */
+
+/* MODO Propiedad slice() -> Extrae una nueva parte de la cadena y devuelve otra cadena.
+                             Es posible meter número negativo y comienza desde atrás. */
+
+// console.log(cadena.slice(0,3));
+// console.log(cadena.slice(5));
+// console.log(cadena.slice(-3));
+
+// ==========================================================================
+
+/* startWith() */
+
+/* MODO Propiedad startWith() -> Comprueba si el String comienza con el caracter que elegimos.*/
+
+// console.log(cadena.startsWith('H'));
+// console.log(cadena.startsWith('h'));
+// console.log(cadena.startsWith('m', 5));
+
+// ==========================================================================
+
+/* endsWith() */
+
+/* MODO Propiedad endsWith('valor') -> Comprueba si el String termina con el caracter que le pasamos*/
+
+// console.log(cadena.endsWith('o'));
+// console.log(cadena.endsWith('a', 4));
+// console.log(cadena.endsWith('n', 8));
+// console.log(cadena.endsWith('Mundo'));
+
+// ==========================================================================
+
+/* includes() */
+
+/* MODO Propiedad includes('valor', [inicio]) -> Igual que el IndexOf() pero devuelve true o false */
+
+// console.log(cadena.includes('n'));
+// console.log(cadena.includes('a', 5));
+// console.log(cadena.includes('a', 2));
+
+// ==========================================================================
+
+/* repeat() */
+
+/* MODO Propiedad repeat(valor) -> Repite el String el número de veces que quieras */
+
+// console.log(cadena.repeat(3));
