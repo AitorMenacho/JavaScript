767b9a1841dd8af9b30333806e94b0346e97e5fb
Aitor Menacho <menacho.aitor@gmail.com>
2022-10-05 08:47:19 +0200

    Nueva actividad

---
+++ Tema3/JS/PruebaObjetos.js
@@ -0,0 +1,44 @@
+
+let cadena = "Hola mundo"
+
+console.log(cadena);
+
+/* Strings */
+
+/* MODO Propiedad Length -> Devuelve la longitud de la cadena. */
+
+// console.log("Longitud: " + cadena.length);
+
+
+/* toUpperCase() y toLowerCase() */
+
+/* MODO Propiedad toUpperCase() -> Devuelve el String en mayuscula */
+
+// console.log(cadena.toUpperCase());
+
+/* MODO Propiedad toLowerCase() -> Devuelve el String en minuscula */
+
+// console.log(cadena.toLocaleLowerCase());
+
+/* indexOf() */
+
+/* MODO Propiedad indexOf() -> Devulve la posición de la letra en la cadena, si no la encuentra devuelve -1 */
+
+// console.log(cadena.indexOf('o'));
+// console.log(cadena.indexOf('mundo'));
+// console.log(cadena.indexOf('d'));
+// console.log(cadena.indexOf('ñ'));
+
+/* replace() */
+
+/* MODO Propiedad replace() -> Reemplaza una subcadena por otra. */
+
+// console.log(cadena.replace(' ', '...'));
+
+/* substring() */
+
+/* MODO Propiedad substring() -> Extrae los caracteres de una cadena entre dos números */
+
+console.log(cadena.substring(0,3));
+
+/*  */
